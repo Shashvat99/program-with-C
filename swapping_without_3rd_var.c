021a275066568bd7f9a1e62b54cb1810ac8921d4
Shashvat <59657437+Shashvat99@users.noreply.github.com>
2021-08-11 12:04:09 +0530

    Add files via upload

---
+++ swapping_without_3rd_var.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+printf("enter the value of a=");
+scanf("%d",&a);
+printf("enter the value of b=");
+scanf("%d",&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("a is %d\n",a);
+printf("b is %d",b);
+}
