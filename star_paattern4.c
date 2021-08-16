ea908d66f606a7dda451d96155cc0097557a51fd
Shashvat <59657437+Shashvat99@users.noreply.github.com>
2021-08-16 14:08:08 +0530

    Add files via upload

---
+++ star_paattern4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int n,i,j;
+printf("enter the number of rows");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+for(j=1; j<=n; j++)
+{
+if(j<=6-i)
+printf("*");
+else
+printf(" ");}
+printf("\n");}}
