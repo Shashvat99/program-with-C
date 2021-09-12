e8905d9196be145b345b1ade7b48232cc3d91754
Shashvat <59657437+Shashvat99@users.noreply.github.com>
2021-09-12 20:45:43 +0530

    Add files via upload

---
+++ enum-1.c
@@ -0,0 +1,23 @@
+enum boolean
+{
+true ,false
+};
+enum boolean iseven(int x)
+{
+if (x%2==0)
+return(true);
+else
+return (false);
+}
+main()
+{
+int n;
+enum boolean result;
+printf("enter a number");
+scanf("%d",&n);
+result=iseven(n);
+if(result==true)
+printf("number is even");
+else
+printf("number is odd");
+}
